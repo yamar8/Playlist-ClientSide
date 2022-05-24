030f064a4c0af937586dd69b0b5051a51cdbf3f4
Yossef <yamar8@gmail.com>
2022-05-24 19:03:48 +0300

    added Route

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 import "./App.css";
 import PathRoot from "./pages/PathRoot";
-=======
-import './App.css';
 
->>>>>>> eb3d13d231ed777a78cd4ed291a28873c155e398
 function App() {
   return (
     <div className="App">
