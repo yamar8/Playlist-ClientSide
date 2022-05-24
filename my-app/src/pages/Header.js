e3d2c3f19e732b25c85ddc23d8bc0124727f4632
נהוראי <nehoraimimon@gmail.com>
2022-05-24 19:04:06 +0300

    add Link

---
+++ my-app/src/pages/Header.js
@@ -0,0 +1,13 @@
+import {Link} from 'react-router-dom';
+
+export default function Header(){
+ return(
+    <>
+    <Link to="/">Login</Link>
+    <Link to="/searchSong">Login</Link>
+    <Link to="/songList">Login</Link>
+    
+    </>
+
+ )
+}
