5820b76ad3c881904a5d2bea03ac001bb32f2d93
Yossef <yamar8@gmail.com>
2022-06-08 11:25:44 +0300

    api

--- my-app/src/pages/PathRoot.js
+++ my-app/src/pages/PathRoot.js
@@ -4,7 +4,7 @@ import SearchSong from "./SearchSong";
 import SongList from "./SongList";
 import {useContext} from 'react';
 import {NameContext} from '../App.js'
-
+import Song from './Song';
   
 
 
@@ -17,8 +17,10 @@ return (
       <Routes>
         <Route path="/" element={<Navigate to="/Login" />} />
         <Route path="/Login" element={<Login />}></Route>
-        <Route path="/searchSong" element={userName? <SearchSong />:<Navigate to="/Login" /> }></Route>
+        <Route path="/searchSong" element={<SearchSong /> }></Route>
+        {/* <Route path="/searchSong" element={userName? <SearchSong />:<Navigate to="/Login" /> }></Route> */}
         <Route path="/songList" element={userName? <SongList />:<Navigate to="/Login" /> }></Route>
+        <Route path="/song" element={<Song/>}></Route>
       </Routes>
     </nav>
   );
--- my-app/src/pages/SearchSong.js
+++ my-app/src/pages/SearchSong.js
@@ -1,7 +1,50 @@
+import {useState,useEffect} from 'react';
+import {Link} from 'react-router-dom';
+
 export default function SearchSong(){
+    
+const [inputValue,setInputValue] = useState("");
+const [result,setResult] = useState();
+const [search,setSearch] = useState();
+
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'simple-youtube-search.p.rapidapi.com',
+            'X-RapidAPI-Key': 'e3ac5866b0msh2e100b947fe0dcdp190da0jsn0c520883eb43'
+        }
+    };
+    
+    useEffect(() => {
+        fetch('https://simple-youtube-search.p.rapidapi.com/search?query=' + search + '&safesearch=false', options)
+            .then(response => response.json())
+            .then(response => {
+                setResult(response.results);
+                 console.log(response)})
+            .catch(err => console.error(err));
+
+    },[search])
+
+    const onClickHandler = ()=>{
+       setSearch(inputValue);
+    }
+    
     return(
-        <>
-        <p>searchSong:</p>
-        </>
+      <div>
+          <input onChange={(e)=>setInputValue(e.target.value)}></input>
+            <button onClick={onClickHandler}>Search</button>
+        <div>
+      {
+            result?.map((v)=>{
+            return (
+            <div>
+            <Link to="/song"> <h1>{v.title}</h1></Link>
+            <img src = {v.thumbnail.url} alt="" className='songImage'/>
+            </div>)
+        })}
+        
+        </div>
+      </div>
     )
 }
